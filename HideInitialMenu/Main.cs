cba65f9fbc5ae0174ffa449dbbc3ab5f3bb2e1b1
My Name <tiaaa0012@gmail.com>
2022-02-06 13:06:29 +0900

    초기 커밋.

---
+++ HideInitialMenu/Main.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace HideInitialMenu
+{
+    public static class Main
+    {
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static Harmony harmony;
+        public static bool IsEnabled = false;
+        public static bool isplaying = false; //OnUpdate에 사용되기 위한 예시
+        public static bool featured = false;
+
+        public static void Setup(UnityModManager.ModEntry modEntry)
+        {
+            Logger = modEntry.Logger;
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnUpdate = OnUpdate; //선택
+        }
+
+        //선택
+        private static void OnUpdate(UnityModManager.ModEntry modentry, float deltaTime)
+        {
+            
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            IsEnabled = value;
+
+            if (value)
+            {
+                //켜질때
+                harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            else
+            {
+                //꺼질때
+                harmony.UnpatchAll(modEntry.Info.Id);
+            }
+            return true;
+        }
+    }
+}
---
+++ HideInitialMenu/Patch.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideInitialMenu
+{
+    internal static class Patcher { 
+        [PatchCondition("HideInitialMenu.scnCLSUpdate", "scnCLS", "Start")]
+        public static class Patch
+        {
+            public static void Prefix(scnCLS __instance)
+            {
+                if (!Main.featured)
+                {
+                    __instance.WorkshopLevelsPortal();
+                }
+                else if (Main.featured)
+                {
+                    Main.featured = false;
+                    __instance.FeaturedLevelsPortal();
+                }
+            }
+        }
+
+        [PatchCondition("HideInitialMenu.scnLevelSelectUpdate","scnLevelSelect","Update")]
+        public static class UpdatePatch
+        {
+            public static void Prefix()
+            {
+                if (RDEditorUtils.CheckForKeyCombo(true,true,UnityEngine.KeyCode.C) || RDEditorUtils.CheckForKeyCombo(true, true, UnityEngine.KeyCode.E)){
+                    return;
+                }
+                else if (RDEditorUtils.CheckForKeyCombo(true, true, UnityEngine.KeyCode.F))
+                {
+                    Main.featured = true;
+                    GCS.sceneToLoad = "scnCLS";
+                    scrUIController.instance.WipeToBlack(WipeDirection.StartsFromRight, null);
+                }
+            }
+        }
+    }
+}
